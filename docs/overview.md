8253ce56bf4205356430e533867cb0c6481fb0dd
Elena Baskakova <elena.baskakova.93@gmail.com>
2022-11-13 19:02:17 +0100

    initial commit

---
+++ docs/overview.md
@@ -0,0 +1,24 @@
+---
+sidebar_position: 1
+---
+
+# Overview
+
+## About the company
+
+Paws & Tails has been providing a digital marketplace and IT consultancy for thousands of pet shops all around the globe since 2013.
+We help our customers with pet shop management using both CMS and REST API as well as with building their websites and mobile apps.
+
+## Paws & Tails API
+
+Paws & Tails provides a REST API - a digital interface for your pet shop data. It allows managing all data related to pets, orders and so on and integrating existing products like mobile apps.
+
+Using this REST API, pet shop staff or client applications can exchange data with the Paws & Tails server.
+
+Data is transferred in text format via the Internet using HTTP requests of different types.
+
+All data is transferred safely using state-of-the-art encryption.
+
+To work with the Paws & Tails API, you need to get an API key.
+
+We recommend reading [Paws & Tails API tutorial](userguide.md) before working with the API reference.
---
+++ docusaurus.config.js
@@ -0,0 +1,106 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "My Site",
+  tagline: "Dinosaurs are cool",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "elenaspb2019", // Usually your GitHub org/user name.
+  projectName: "api-test", // Usually your repo name.
+  deploymentBranch: "gh-pages",
+  
+
+  presets: [
+    [
+      "docusaurus-preset-openapi",
+      /** @type {import('docusaurus-preset-openapi').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('docusaurus-preset-openapi').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Paws & Tails",
+        logo: {
+          alt: "My Site Logo",
+          src: "img/logo.svg",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "overview",
+            position: "left",
+            label: "Documentation",
+          },
+          { to: "/api", label: "API", position: "left" },
+          {
+            href: "https://github.com/facebook/docusaurus",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Overview",
+                to: "/docs/overview",
+              },
+              {
+                label: "How to use Paws & Tails API",
+                to: "/docs/userguide",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
+              },
+              {
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
